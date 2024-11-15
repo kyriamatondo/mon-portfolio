55c3e03d93da13a8553078c83a11a73247ed7af9
kyriamatondo <krmatondo@gmail.com>
2024-11-15 08:20:08 +0100

    modification

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log("Salut");
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector("nav ul");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("visible");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("visible");
+  });
+});
